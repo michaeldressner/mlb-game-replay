9a1fc8f7bf877ff2c4394943f265a247615d226f
mdressne <mdressne@umd.edu>
2020-09-08 12:57:18 -0400

    wrote a test in the main file

--- mlb_test.py
+++ mlb_test.py
@@ -29,4 +29,11 @@ gl_file = open('GL' + str(year) + '.TXT', 'r')
 for line in csv.reader(gl_file, delimiter = ","):
     game = Game(line)
 
+    teams[game.v_team].add_stat('h', game.v_h)
+    teams[game.h_team].add_stat('h', game.h_h)
+
+for team in teams:
+    print("Team:", teams[team].city + " " + teams[team].nickname,
+            "Hits:", teams[team].season.h)
+
 gl_file.close()
