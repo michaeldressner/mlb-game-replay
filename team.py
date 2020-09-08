75e0c27af4b9f8ecc3f95a6d8da11153df42ebc3
mdressne <mdressne@umd.edu>
2020-09-07 20:58:29 -0400

    added team fields to Team class

--- team.py
+++ team.py
@@ -1,4 +1,8 @@
 class Team():
-    def __init__(self, team_id):
+    def __init__(self, team_id, league, city, nickname, first_year, last_year):
         self.id = team_id
-        self.name = ""
+        self.league = league
+        self.city = city
+        self.nickname = nickname
+        self.first_year = first_year
+        self.last_year = last_year
