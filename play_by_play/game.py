9293eda8f66d6ac9d2fe2ad7c99192a9d6796331
mdressne <mdressne@umd.edu>
2020-09-09 18:26:05 -0400

    added some fields to Game class

--- play_by_play/game.py
+++ play_by_play/game.py
@@ -1,6 +1,32 @@
 class Game():
     def __init__(self):
-        pass
+        self.v_team = ''
+        self.h_team = ''
+        self.site = ''
+        self.date = ''
+        self.number = ''
+        self.start_time = ''
+        self.day = ''
+        self.use_dh = ''
+        self.ump_home = ''
+        self.ump_1b = ''
+        self.ump_2b = ''
+        self.ump_3b = ''
+        self.pitches = ''
+        self.oscorer = ''
+        self.temp = ''
+        self.wind_dir = ''
+        self.wind_speed = ''
+        self.field_cond = ''
+        self.precip = ''
+        self.sky = ''
+        self.time_of_game = ''
+        self.attendance = ''
+        self.wp = ''
+        self.lp = ''
+        self.save = ''
+        self.v_starters = list()
+        self.h_starters = list()
 
     def __repr__(self):
         return ''
